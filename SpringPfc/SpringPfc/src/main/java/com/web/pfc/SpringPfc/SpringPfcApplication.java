96fad1ff0b559fa3aeb14bf0c3667c47b193521e
Juliana Ferreira Zanelato <julianazanelato1027@gmail.com>
2026-09-09 23:25:35 -0300

    Add files via upload

---
+++ SpringPfc/SpringPfc/src/main/java/com/web/pfc/SpringPfc/SpringPfcApplication.java
@@ -0,0 +1,13 @@
+package com.web.pfc.SpringPfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPfcApplication.class, args);
+	}
+
+}
---
+++ SpringPfc/SpringPfc/src/test/java/com/web/pfc/SpringPfc/SpringPfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.pfc.SpringPfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
