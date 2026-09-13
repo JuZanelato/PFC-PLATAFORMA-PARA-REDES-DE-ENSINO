a0a3c89eb266ec94f13b5539b3532dd84138db23
Juliana Ferreira Zanelato <julianazanelato1027@gmail.com>
2026-09-12 21:43:54 -0300

    CRUD INSTITUIÇÃO
    Entrega Back Juliana

---
+++ SpringPfc/SpringPfc/src/main/java/com/web/pfc/SpringPfc/Repository/InstituicaoRep.java
@@ -0,0 +1,11 @@
+package com.web.pfc.SpringPfc.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web.pfc.SpringPfc.domain.Instituicao;
+
+@Repository
+public interface InstituicaoRep extends JpaRepository<Instituicao, Integer> {
+    
+}
