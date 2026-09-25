f5d50f6d99f0c067d518e749001640dbe6f84f2f
Gustavo-Iwai <gustavo.iwai@hotmail.com>
2026-09-25 17:37:31 -0300

    Metodo de buscar logs

--- SpringPfc/SpringPfc/src/main/java/com/web/pfc/SpringPfc/Repository/LogAuditoriaRep.java
+++ SpringPfc/SpringPfc/src/main/java/com/web/pfc/SpringPfc/Repository/LogAuditoriaRep.java
@@ -1,7 +1,10 @@
 package com.web.pfc.SpringPfc.Repository;
 
 import com.web.pfc.SpringPfc.domain.LogAuditoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LogAuditoriaRep extends JpaRepository<LogAuditoria, Long> {
+    Page<LogAuditoria> findAllByOrderByDataHoraDesc(Pageable pageable);
 }
